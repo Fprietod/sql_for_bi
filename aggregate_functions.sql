e00b2b7a2a0991e5624f0994611a29186c537931
F.Prieto <felipe00@live.com.mx>
2021-01-22 01:00:00 -0600

    Create aggregate_functions.sql

---
+++ aggregate_functions.sql
@@ -0,0 +1,5 @@
+/* Max and min
+MAX() takes the name of a column as an argument and returns the largest value in that column. Here, we returned the largest value in the downloads column.
+
+MIN() works the same way but it does the exact opposite; it returns the smallest value.
+*/

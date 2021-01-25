5f8202df9621257d48e081ee3d36db195976992e
F.Prieto <felipe00@live.com.mx>
2021-01-25 13:58:20 -0600

    Update subquery.sql

--- subquery.sql
+++ subquery.sql
@@ -33,6 +33,12 @@ Previously, we mentioned subqueries can be placed inside of SELECT, INSERT, UPDA
 In the same way that the external query selects from the internal query’s results, it is important to note that this same behavior takes place when the external query is an INSERT, UPDATE, or DELETE. Therefore, when a subquery is nested in a DELETE statement, the rows to be deleted will be among the results from the subquery. 
 For example, suppose students are unable to take both history and statistics. If we wanted to delete the rows for statistics students who are also enrolled in history, we could execute a statement such as: 
 */
+Select grade
+FROM band_students
+WHERE EXISTS (
+  SELECT grade
+  FROM drama_students
+);
 DELETE FROM statistics_students
 WHERE id in (
   SELECT id 
@@ -54,3 +60,36 @@ WHERE grade = (
    SELECT grade
    FROM band_students
    WHERE id = 20);
+   /* 
+   In and Not In Clauses
+
+One of the more common ways to use subqueries is with the use of an IN or NOT IN clause. Recall that the subquery is always executed first followed by the external query.
+
+When an IN clause is used, results retrieved from the external query must appear within the subquery results. Similarly, when a NOT IN clause is used, results retrieved from the external query must not appear within the subquery results. 
+
+For example, we could use the below query to find out which students are enrolled in statistics and history:
+*/
+SELECT * 
+FROM statistics_students
+WHERE id 
+IN (
+  SELECT id
+  FROM history_students);
+ 
+ /*
+ Write a query that gives the first and last names of students enrolled in band but not in drama.
+ */
+ SELECT first_name, last_name
+FROM band_students
+WHERE id NOT IN (
+   SELECT id
+   FROM drama_students);
+ /*
+ Exists and Not Exists
+
+In the previous exercise we discussed the use of IN and NOT IN clauses. We also have the option of EXISTS and NOT EXISTS clauses. While EXISTS/NOT EXISTS are similar to IN/NOT IN clauses, there are some key differences. Let’s explore those differences now.
+
+If we compare this functionality in terms of efficiency, EXISTS/NOT EXISTS are usually more efficient than IN/NOT IN clauses; this is because the IN/NOT IN clause has to return all rows meeting the specific criteria whereas the EXISTS/NOT EXISTS only needs to find the presence of one row to determine if a true or false value needs to be returned.
+Write a query to find out which grade levels are represented in both band and drama.
+/*
+

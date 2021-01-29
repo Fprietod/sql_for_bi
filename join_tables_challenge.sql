bf16e6c2c25815aeec2606700a239499e169937b
F.Prieto <felipe00@live.com.mx>
2021-01-28 20:46:06 -0600

    Update join_tables_challenge.sql

--- join_tables_challenge.sql
+++ join_tables_challenge.sql
@@ -1,1 +1,21 @@
 
+SELECT premium_users.user_id,
+   plans.description
+FROM premium_users
+JOIN plans
+  ON plans.id = premium_users.membership_plan_id;
+  
+  /*played by each user!
+
+The column song_id in plays should match the column id in songs.
+
+Join plays to songs and select:
+
+    user_id from plays
+    play_date from plays
+    title from songs
+*/
+SELECT plays.user_id, plays.play_date, songs.title FROM
+ songs
+ JOIN plays
+ ON plays.song_id = songs.id;

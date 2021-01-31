41bcd1b46b75ce3619f48f22817647318ab025f2
F.Prieto <felipe00@live.com.mx>
2021-01-31 00:54:22 -0600

    Update build_funnel.sql

--- build_funnel.sql
+++ build_funnel.sql
@@ -1,1 +1,20 @@
+/*What is a Funnel?
 
+In the world of marketing analysis, “funnel” is a word you will hear time and time again.
+
+A funnel is a marketing model which illustrates the theoretical customer journey towards the purchase of a product or service. Oftentimes, we want to track how many users complete a series of steps and know which steps have the most number of users giving up. 
+
+We will be using a table called survey_responses with the following columns:
+
+    question_text - the survey question
+    user_id - the user identifier
+    response - the user answer
+
+Count the number of distinct user_id who answered each question_text.
+
+You can do this by using a simple GROUP BY command.
+*/
+ SELECT question_text,
+ COUNT(DISTINCT user_id)
+ FROM survey_responses
+ GROUP BY 1;

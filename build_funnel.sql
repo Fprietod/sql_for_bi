b573404dceb5514cb82caa5a1557f0c2196e7cd5
F.Prieto <felipe00@live.com.mx>
2021-02-01 22:42:38 -0600

    Update build_funnel.sql

--- build_funnel.sql
+++ build_funnel.sql
@@ -65,5 +65,62 @@ ORDER BY 1;
 FROM onboarding_modals
 GROUP BY modal_text
 ORDER BY modal_text;
+     /*
+                                  Build a Funnel from Multiple Tables 1
+
+Scenario: Mattresses and More sells bedding essentials from their e-commerce store. Their purchase funnel is:
+
+    The user browses products and adds them to their cart
+    The user proceeds to the checkout page
+    The user enters credit card information and makes a purchase
+
+Three steps! Simple and easy.
+
+As a sales analyst, you want to examine data from the shopping days before Christmas. As Christmas approaches, you suspect that customers become more likely to purchase items in their cart (i.e., they move from window shopping to buying presents).
+                                  */
+   /*
+             Each row will represent a single user:
+
+    If the user has any entries in checkout, then is_checkout will be True.
+    If the user has any entries in purchase, then is_purchase will be True.
+
+If we use an INNER JOIN to create this table, we’ll lose information from any customer who does not have a row in the checkout or purchase table.
+
+Therefore, we’ll need to use a series of LEFT JOIN commands.
+                                  */
+                                  
+      SELECT DISTINCT b.browse_date,
+ b.user_id, c.user_id IS NOT NULL AS is_checkout, p.user_id IS NOT NULL AS is_purchase
+ FROM browse AS 'b'
+ LEFT JOIN checkout AS 'c'
+ ON c.user_id = b.user_id
+ LEFT JOIN purchase AS 'p'
+ ON p.user_id = c.user_id
+ LIMIT 50;
+                                  
+                                  /*
+                                  Finally, let’s do add some more calculations to make the results more in depth.
+
+Let’s add these two columns:
+
+    Percentage of users from browse to checkout
+    Percentage of users from checkout to purchase
+*/
+   WITH funnels AS (
+  SELECT DISTINCT b.browse_date,
+     b.user_id,
+     c.user_id IS NOT NULL AS 'is_checkout',
+     p.user_id IS NOT NULL AS 'is_purchase'
+  FROM browse AS 'b'
+  LEFT JOIN checkout AS 'c'
+    ON c.user_id = b.user_id
+  LEFT JOIN purchase AS 'p'
+    ON p.user_id = c.user_id)
+SELECT COUNT(*) as num_browse,
+SUM(is_checkout) as num_checkout,
+SUM(is_purchase) as num_purchase,
+1.0 * SUM(is_checkout)/ COUNT(user_id) as porciento,
+1.0 * SUM(is_purchase) / SUM(is_checkout)
+FROM funnels;
                               
                                   

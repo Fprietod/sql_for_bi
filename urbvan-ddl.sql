52c7407bed7557b42b4ee7a3192e79c54bfa2733
F.Prieto <felipe00@live.com.mx>
2021-02-16 15:19:38 -0600

    Update urbvan-ddl.sql

--- urbvan-ddl.sql
+++ urbvan-ddl.sql
@@ -1,20 +1,6 @@
--- MySQL Workbench Forward Engineering
-
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
-
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
-
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
 USE `mydb` ;
 
--- -----------------------------------------------------
 -- Table `mydb`.`client`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `mydb`.`client` (
@@ -92,7 +78,3 @@ CREATE TABLE IF NOT EXISTS `mydb`.`reservation` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;

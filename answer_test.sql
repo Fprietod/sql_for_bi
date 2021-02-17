7d6dbeec98adf85b7a11d1383ea1db187ef0feb9
F.Prieto <felipe00@live.com.mx>
2021-02-17 02:10:18 -0600

    Update answer_test.sql

--- answer_test.sql
+++ answer_test.sql
@@ -12,6 +12,24 @@ limit 10;
 --How many trips have more seats reserved than their vehicle_capacity?
 select count(*) from reservation r, trip t 
 where r.seats > t.vehicle_capacity;
-/* Esta opción puede que sea un poco lenta pero comparamos el número de asientos reservados, con la capacidad de cada vehículo en el viaje/*
+/* Esta opción puede que sea un poco lenta pero comparamos el número de asientos reservados, con la capacidad de cada vehículo en el viaje*/
+/* How many different full names (first_name + last_name) are in tables client & driver
+combined.
+*/
+with full_name AS (
+select  concat(d.first_name, ' ', d.last_name) as nombre_driver from driver d
+)
+select count(distinct(nombre_driver)) from full_name;
+                      -- Primero se tuvo que dividir este query en varias partes, se intento hacer en uno solo, pero por la complejidad
+                      -- Se separaron los resultados, creando dos tablas temporales donde se alojaba el resultado y al último combinando los resultados
+
+with client_name as(
+select concat(c.first_name, ' ', c.last_name) as nombre_client from client c
+)
+                      -- Esta tabla temporal almacena todos los nombres completos de los clientes
+select count(distinct (nombre_client)) from client_name; --aquí ya filtramos los nombres que sean repetidos para reducir la carga
+
+
+select sum(16048+967);
 
 

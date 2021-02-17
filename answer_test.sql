e6ab1a04996412295db888228da5bd3135858806
F.Prieto <felipe00@live.com.mx>
2021-02-16 23:21:27 -0600

    Update answer_test.sql

--- answer_test.sql
+++ answer_test.sql
@@ -1,2 +1,5 @@
 select count(client_id) from client;
 --30,000
+/* La segunda pregunta es un poco ambigua, necesitamos conocer más por ejemplo, el día que se gasto más, ya que el máximo que un cliente pudo haber gastado es 195
+Eso si lo hacemos en una suma total, sin embargo interesaría saber el día en específico que se hizo la reservación para poder tener un punto de partida sobre nuestros clientes
+/*

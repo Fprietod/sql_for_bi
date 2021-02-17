dab8a517a1cb6f31ee4f2b1e503120292b73937e
F.Prieto <felipe00@live.com.mx>
2021-02-17 00:18:58 -0600

    Update answer_test.sql

--- answer_test.sql
+++ answer_test.sql
@@ -1,5 +1,11 @@
 select count(client_id) from client;
 --30,000
-/* La segunda pregunta es un poco ambigua, necesitamos conocer más por ejemplo, el día que se gasto más, ya que el máximo que un cliente pudo haber gastado es 195
-Eso si lo hacemos en una suma total, sin embargo interesaría saber el día en específico que se hizo la reservación para poder tener un punto de partida sobre nuestros clientes
-/*
+-- Which are the 10 clients that have spent more money with their reservations?
+-- La manera en que se resolvio esto fue igualando el id del cliente, con sus diferentes reservaciones y así sumar sus costos totales
+select c.first_name , c.last_name,sum(r.total_price) as maximo_monto_gastado
+from reservation r, client c
+where c.client_id = r.client_id
+group by c.first_name, c.last_name 
+order by maximo_monto_gastado desc 
+limit 10;
+
